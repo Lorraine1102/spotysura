c550692018289b5b5c70193af5e36a4bb6065bee
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-02-27 15:10:27 -0500

    se crea spotysura

---
+++ src/controllers/controlingreso.js
@@ -0,0 +1,12 @@
+//DECLARANDO VARIABLES CON JS DESDE EL DOM
+
+let nombreUsurio="Lorena Conto Moreno"
+let edadUsuario=34
+let estaturaUsuario=1.65
+let estaLloviendo=false
+let rh="O+" 
+
+let botonFormulario=document.getElementById("botonIngreso")
+let cajaFomularioNombre=document.getElementById("nombre")
+let cajaFomularioEmail=document.getElementById("correo")
+let cajaFomularioContrseña=document.getElementById("password")
